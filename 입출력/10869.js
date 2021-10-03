3c06a145a084125ef18cdb8d5cf3f20f0b25259d
hjinn <hojun@hojun.local>
2021-10-03 22:26:49 +0900

    if문

--- 입출력/10869.js
+++ 입출력/10869.js
@@ -1,7 +1,6 @@
 let fs = require('fs');
 let input = fs.readFileSync('input.txt').toString().split(' ');
 // let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-console.log(Number(input[0]), Number(input[1]));
 console.log(Number(input[0]) + Number(input[1]));
 console.log(Number(input[0]) - Number(input[1]));
 console.log(Number(input[0]) * Number(input[1]));
